4cf7f2fdd6d40220a8b5379fadd39f334b1df04c
Arib Nawal <65786174+arib168@users.noreply.github.com>
2021-12-10 17:47:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+%%writefile app.py 
+import streamlit as st 
+import cv2
+import pytesseract
+from PIL import Image  #Python Imaging library #to open images 
+import numpy as np 
+
+st.title("Optical Character Recognition (OCR) :")
+st.text("Upload the Image :")
+
+uploaded_file = st.sidebar.fileuploader("Choose an image :", type =["jpg","png","jpeg"])
+if uploaded_file is not None: #if there is some file uploaded here, then do the following 
+  img = Image.open(uploaded_file) #reads the file uploaded (similar to cv2.imread)
+  st.write("") 
+  if st.button("PREDICT"):
+    st.write("Result :")
+    info = pytesseract.image_to_string(img) #ocr is applied using pytesseract
+    st.title(info)
